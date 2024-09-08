8e2184aaefe93f05aa77e8c819a2258e83f39b42
¨Gonzalo <albarracin.agn@gmail.com>
2024-09-08 00:52:02 -0300

    Update-[v0.0.5]-Final

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # numeros-primos-GALBARRACIN
-Alumno: Gonzalo Nahuel Albarracin
-Legajo: 63407
+## Alumno: Gonzalo Nahuel Albarracin
+## Legajo: 63407
 
 En los archivos se visualiza el codigo visto en clase. El archivo "numeros_primos_propio" es un codigo de prueba con su test correspondiente al igual que sus respectivos comentarios de su funcionamiento.
 
@@ -9,5 +9,12 @@ En los archivos se visualiza el codigo visto en clase. El archivo "numeros_primo
 - Anteriormente el archivo llego a los commits deseados pero el repositorio completo fue eliminado , ya que en su momento al pushear en la rama main daba error y se creo la rama master.
 
 - Se corrigió y se agrego pruebas unitarias.
+
+
+## Tests
+
+Para ejecutar el test:
+
+" python -m unittest test_primo_propio.py "
 
 
--- test_primo_propio.py
+++ test_primo_propio.py
@@ -1,3 +1,8 @@
+import unittest  # Importamos el módulo unittest para crear pruebas unitarias.
+from io import StringIO  # Importamos StringIO para capturar la salida impresa.
+from contextlib import redirect_stdout  # Importamos redirect_stdout para redirigir la salida estándar.
+from numeros_primos_propio import es_primo, mostrar_primos  # Importamos las funciones a probar.
+
 # Definimos una clase de pruebas que hereda de unittest.TestCase.
 class TestNumerosPrimos(unittest.TestCase):
 
@@ -8,3 +13,22 @@ class TestNumerosPrimos(unittest.TestCase):
         self.assertFalse(es_primo(4))  # Verificamos que 4 no es primo.
         self.assertTrue(es_primo(5))  # Verificamos que 5 es primo.
         self.assertFalse(es_primo(10))  # Verificamos que 10 no es primo.
+
+    # Prueba para la función mostrar_primos.
+    def test_mostrar_primos(self):
+        # Capturamos la salida estándar utilizando StringIO.
+        f = StringIO()
+
+        # Redirigimos la salida estándar para capturar la impresión de los números primos.
+        with redirect_stdout(f):
+            mostrar_primos(10)  # Llamamos a la función con 10 números primos.
+
+        # Obtenemos la salida capturada.
+        output = f.getvalue()
+
+        # Verificamos que la salida capturada sea la esperada.
+        self.assertEqual(output.strip(), '2 3 5 7 11 13 17 19 23 29')
+
+# Este bloque asegura que las pruebas se ejecuten cuando este archivo se ejecute directamente.
+if __name__ == '__main__':
+    unittest.main()  # Ejecuta todas las pruebas definidas.
